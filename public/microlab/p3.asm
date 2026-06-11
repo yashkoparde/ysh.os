3ef3865f8cc39d764ec74820edb29fb8f9dc3bad
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-06-11 07:52:36 +0530

    Update p3.asm

--- public/microlab/p3.asm
+++ public/microlab/p3.asm
@@ -1,22 +1,22 @@
 ;Sum of First 10 Integer Numbers
-AREA ADD1TO10, CODE, READONLY
-ENTRY
+        AREA    ADD1TO10, CODE, READONLY
+        ENTRY
 
-MOV R1,#10
-LDR R2,=ARRAY
-MOV R4,#0
+        MOV     R1, #10            ; Length of array
+        LDR     R2, =ARRAY         ; Load starting address of array
+        MOV     R4, #0             ; Initialize sum
 
-NEXT
-LDR R3,[R2],#4
-ADD R4,R4,R3
-SUBS R1,R1,#1
-BNE NEXT
+NEXT    LDR     R3, [R2], #4       ; Load array element and increment pointer
+        ADD     R4, R4, R3         ; Add element to sum
 
-MOV R5,#0X40000000
-STR R4,[R5]
+        SUBS    R1, R1, #1         ; Decrement counter
+        BNE     NEXT               ; Repeat until R1 = 0
 
-STOP B STOP
+        MOV     R5, #0x40000000    ; Memory location for result
+        STR     R4, [R5]           ; Store sum at address 0x40000000
 
-ARRAY DCD 1,2,3,4,5,6,7,8,9,10
+STOP    B       STOP
 
-END
+ARRAY   DCD     1,2,3,4,5,6,7,8,9,10
+
+        END
