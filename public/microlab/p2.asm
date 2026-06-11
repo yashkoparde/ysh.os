ff98a19eaca66df31ec4da1f9b276940803a6d70
YASH B KOPARDE <yashkoparde2022@gmail.com>
2026-06-11 07:52:09 +0530

    Update p2.asm

--- public/microlab/p2.asm
+++ public/microlab/p2.asm
@@ -1,20 +1,22 @@
 ;Multiplication of Two 16-bit Binary Numbers
-AREA Multiply, CODE, READONLY
-ENTRY
+        AREA    Multiply, CODE, READONLY
+        ENTRY
 
-LDR R0,=NUM1
-LDRH R1,[R0]
-LDRH R2,[R0,#2]
-MUL R3,R1,R2
+        LDR     R0, =NUM1          ; Load address of NUM1
+        LDRH    R1, [R0]           ; Load first number
+        LDRH    R2, [R0, #2]       ; Load second number
 
-LDR R4,=NUM2
-LDRH R5,[R4]
-LDRH R6,[R4,#2]
-SMULLS R7,R8,R5,R6
+        MUL     R3, R1, R2         ; R3 = R1 × R2
 
-STOP B STOP
+        LDR     R4, =NUM2          ; Load address of NUM2
+        LDRH    R5, [R4]           ; Load first number
+        LDRH    R6, [R4, #2]       ; Load second number
 
-NUM1 DCW 0X1222,0X1133
-NUM2 DCW 0XFFFF,0XFFFF
+        SMULLS  R7, R8, R5, R6     ; R8:R7 = R5 × R6 (64-bit result)
 
-END
+STOP    B       STOP
+
+NUM1    DCW     0x1222, 0x1133     ; Numbers to multiply
+NUM2    DCW     0xFFFF, 0xFFFF
+
+        END
